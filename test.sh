88cb332bd9ca495c308a17e739ea9943941caaf4
Nathan Rajlich <nathan@tootallnate.net>
2018-09-03 21:43:24 -0700

    Simplify tests
    Cause `set -e` is enabled, no need for all these checks

--- test.sh
+++ test.sh
@@ -5,10 +5,6 @@ source "./assert.sh"
 
 # assert
 assert foo = foo
-if [ $? -ne 0 ]; then
-  echo "assert does not work"
-  exit 1
-fi
 
 r=0
 output="$(assert 1 -eq 2 2>&1)" || r=$?
@@ -29,10 +25,9 @@ assert_equal "$output" "assertion failed: it was bad"
 
 # assert_equal
 assert_equal foo foo
-if [ $? -ne 0 ]; then
-  echo "assert_equal does not work"
-  exit 1
-fi
+assert_equal "a b" "a b"
+assert_equal "&" "&"
+assert_equal "!" "!"
 
 r=0
 output="$(assert_equal foo bar 2>&1)" || r=$?
@@ -50,41 +45,14 @@ if [ $r -ne 1 ]; then
 fi
 assert_equal "$output" "assertion failed: 1 does not equal 2"
 
-assert_equal "a b" "a b"
-if [ $? -ne 0 ]; then
-  echo "assert_equal does not work"
-  exit 1
-fi
-
-assert_equal "&" "&"
-if [ $? -ne 0 ]; then
-  echo "assert_equal does not work"
-  exit 1
-fi
-
-assert_equal "!" "!"
-if [ $? -ne 0 ]; then
-  echo "assert_equal does not work"
-  exit 1
-fi
-
 
 # assert_exit
 assert_exit 1 sh -c "exit 1"
-if [ $? -ne 0 ]; then
-  echo "assert_exit does not work"
-  exit 1
-fi
+assert_exit 0 echo foo
 
 r=0
 output="$(assert_exit 0 sh -c "exit 1" 2>&1)" || r=$?
 if [ $r -ne 1 ]; then
-  echo "assert_exit does not work"
-  exit 1
-fi
-
-assert_exit 0 echo foo
-if [ $? -ne 0 ]; then
   echo "assert_exit does not work"
   exit 1
 fi
